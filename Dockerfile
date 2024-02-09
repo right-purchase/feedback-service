7e58de83a85749dec5d83f01fe9c8b78558df4ad
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2024-02-09 17:40:23 -0300

    Refactor

--- Dockerfile
+++ Dockerfile
@@ -16,11 +16,8 @@ COPY . .
 # Compile TypeScript to JavaScript
 RUN npm run build
 
-# Define an environment variable for the application port with a default value
-ENV EXPRESS_API_PORT=3000
-
 # Expose the application port
-EXPOSE $EXPRESS_API_PORT
+EXPOSE 80
 
 # Start the application
 CMD [ "npm", "start" ]
--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -2,7 +2,7 @@ import express, { IRouterHandler, Router } from "express";
 
 export const startServer = (...routers: Router[]) => {
   const app = express();
-  const port = process.env.EXPRESS_API_PORT || 3000;
+  const port = process.env.EXPRESS_API_PORT || 80;
 
   app.get("/", (_req, res) => {
     res.send("Hello World!");
@@ -11,6 +11,12 @@ export const startServer = (...routers: Router[]) => {
   if (routers.length > 0) {
     app.use("/", ...routers);
   }
+
+  // Add this default route here
+  app.use("*", (req, res) => {
+    console.log(`Unsuccessful attempt: ${req.method} ${req.originalUrl}`);
+    res.status(404).send("Sorry, that route does not exist.");
+  });
 
   app.listen(port, () => {
     console.log(`Server running at port ${port}`);
